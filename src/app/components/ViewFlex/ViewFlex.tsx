9f1b3b6620bc4f79c99e9998337416ffee0a623b
angel <96trinidad@gmail.com>
2021-07-01 19:55:32 -0400

    lint(app): fix

---
+++ src/app/components/ViewFlex/ViewFlex.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, ViewProps } from 'react-native';
+
+import styles from './styles';
+
+type Props = ViewProps & {
+  children?: React.ReactElement;
+};
+
+const ViewFlex: React.FC<Props> = ({ children, ...rest }) => {
+  return (
+    <View style={styles.container} {...rest}>
+      {children}
+    </View>
+  );
+};
+
+export default React.memo(ViewFlex);
